e34f80fe86c7923791e22a8b524fbed76f33b781
崩坏百裂拳 <fuhua@MacBook-Pro.local>
2018-05-30 00:19:11 +0800

    Site updated: 2018-05-30 00:19:11

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,17 @@
-// build time:Tue May 29 2018 23:18:20 GMT+0800 (CST)
-const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,listMaxHeight:"210px",listFolded:true,audio:[{name:"sub/objective",artist:"ぼくのりりっくのぼうよみ",url:"http://music.163.com/song/media/outer/url?id=39449876.mp3",cover:"http://p1.music.126.net/4pCNnLKu0m3qGTIhv70YOQ==/3246857840729730.jpg"}]});
-//rebuild by neat 
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  autoplay: false,
+  listMaxHeight: '210px',
+  listFolded: true,
+  // lrcType: 1,
+  audio: [
+    {
+      name: "sub/objective",
+      artist: 'ぼくのりりっくのぼうよみ',
+      url: 'http://music.163.com/song/media/outer/url?id=39449876.mp3',
+      cover: 'http://p1.music.126.net/4pCNnLKu0m3qGTIhv70YOQ==/3246857840729730.jpg',
+      // lrc: '[00:30.59]架けた桥　远ざかり　远回り\r\n[00:33.39]behave like a blindness\r\n[00:35.54]取り返しつかない所まで\r\n[00:38.04]一人ベットで毛布に包まり\r\n[00:39.87]告げる现実にgood-bye\r\n[00:41.76]数倍に膨らんだ悪梦\r\n[00:43.57]简単に物事を投げ出し\r\n[00:45.42]no matter what I do\r\n[00:46.65]全て未来は决まってる\r\n[00:49.22]そんな振りをした\r\n[00:51.51]そして夜が访れた\r\n[00:53.61]悲しみが空を覆った\r\n[00:56.09]放り出されたまっくらやみ\r\n[00:58.13]见失わないように心研ぎ澄まし\r\n[01:00.78]涂りつぶすグレーにna na na na\r\n[01:03.79]夜中にふと気づくisolation\r\n[01:09.12]なんか泣きたくなるな\r\n[01:12.99]Afterglow　照らすオレンジ\r\n[01:15.90]Afterwards　全て终われば\r\n[01:18.55]泣いたりしなくて済むのかな\r\n[01:21.20]Afterglow　照らすオレンジ \r\n[01:23.39]Afterwards　全て终われば\r\n[01:26.18]泣いたりしなくて済むのかな\r\n[01:29.62]I deceived other everyone\r\n[01:34.33]\r\n[01:41.11]いつしかすり替わる一人称から三人称へ\r\n[01:45.18]二元论でしか世界を観れないのは哀しい\r\n[01:48.15]全てにapathyだから魂夺われて融ける\r\n[01:51.67]いつしか物を见ている自分を见るようになった\r\n[01:55.72]人からどう见えてんのか　それだけ気にしてる\r\n[01:59.27]なんて素晴らしい人生だろう\r\n[02:01.67]“Check this！！”“Check this！！”\r\n[02:02.84]叫ぶ自分をobjectiveに眺める\r\n[02:05.66]急にどうでも良くなって　投げ出す何もかも\r\n[02:08.62]そして拾い集める　また泣いてるよ\r\n[02:11.71]\r\n[02:14.28]Daybreak　また夜が明ける\r\n[02:16.69]Describe　仆を贯く\r\n[02:19.54]涙で何か変わるかなぁ\r\n[02:21.80]Daybreak　また夜が明ける\r\n[02:24.21]Describe　仆を贯く\r\n[02:26.99]涙で何か変わるかなぁ\r\n[02:30.36]I deceived even myself...\r\n[02:33.17]'
+    }
+  ]
+});
